96519f44f4235afc2c23277b6b3d124f5a669c7a
Albertas Vyšniauskas <thezbyg@gmail.com>
2016-03-26 12:27:41 +0200

    Add AppData file.
    Fixes #117

--- source/uiAbout.cpp
+++ source/uiAbout.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2015, Albertas Vyšniauskas
+ * Copyright (c) 2009-2016, Albertas Vyšniauskas
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -37,7 +37,7 @@ static GtkWidget* new_page(const char *text)
 void show_about_box(GtkWidget *widget)
 {
 	const char *license = {
-"Copyright \xc2\xa9 2009-2015, Albertas Vyšniauskas\n"
+"Copyright \xc2\xa9 2009-2016, Albertas Vyšniauskas\n"
 "\n"
 "All rights reserved.\n"
 "\n"
@@ -69,7 +69,7 @@ void show_about_box(GtkWidget *widget)
 "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n"
 	};
 	const char *program_authors = {
-"Albertas Vyšniauskas <thezbyg@gmail.com>\n"
+"Albertas Vyšniauskas <albertas.vysniauskas@gpick.org>\n"
 /* Add yourself here if you helped Gpick project in any way (patch, translation, etc).
  * Everything is optional, if you do not want, you do not have to disclose your e-mail
  * address, real name or any other information.
@@ -101,7 +101,7 @@ void show_about_box(GtkWidget *widget)
 	gtk_label_set_selectable(GTK_LABEL(comments), true);
 	gtk_label_set_justify(GTK_LABEL(comments), GTK_JUSTIFY_CENTER);
 	gtk_box_pack_start(GTK_BOX(vbox2), comments, false, false, 0);
-	tmp_string = g_markup_printf_escaped ("<span size=\"small\">%s</span>", _("Copyrights © 2009-2015, Albertas Vyšniauskas and Gpick development team"));
+	tmp_string = g_markup_printf_escaped ("<span size=\"small\">%s</span>", _("Copyrights © 2009-2016, Albertas Vyšniauskas and Gpick development team"));
 	GtkWidget *copyright = gtk_label_new(0);
 	gtk_label_set_selectable(GTK_LABEL(copyright), true);
 	gtk_label_set_justify(GTK_LABEL(copyright), GTK_JUSTIFY_CENTER);
