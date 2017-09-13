bf7ef2fb3ecfbe2196e8a360072f70622ef35dc8
Albertas Vyšniauskas <thezbyg@gmail.com>
2017-09-13 22:46:37 +0300

    Update about dialog.

--- source/uiAbout.cpp
+++ source/uiAbout.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2016, Albertas Vyšniauskas
+ * Copyright (c) 2009-2017, Albertas Vyšniauskas
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -37,7 +37,7 @@ static GtkWidget* new_page(const char *text)
 void show_about_box(GtkWidget *widget)
 {
 	const char *license = {
-"Copyright \xc2\xa9 2009-2016, Albertas Vyšniauskas\n"
+"Copyright \xc2\xa9 2009-2017, Albertas Vyšniauskas\n"
 "\n"
 "All rights reserved.\n"
 "\n"
@@ -101,7 +101,7 @@ void show_about_box(GtkWidget *widget)
 	gtk_label_set_selectable(GTK_LABEL(comments), true);
 	gtk_label_set_justify(GTK_LABEL(comments), GTK_JUSTIFY_CENTER);
 	gtk_box_pack_start(GTK_BOX(vbox2), comments, false, false, 0);
-	tmp_string = g_markup_printf_escaped ("<span size=\"small\">%s</span>", _("Copyright © 2009-2016, Albertas Vyšniauskas and Gpick development team"));
+	tmp_string = g_markup_printf_escaped ("<span size=\"small\">%s</span>", _("Copyright © 2009-2017, Albertas Vyšniauskas and Gpick development team"));
 	GtkWidget *copyright = gtk_label_new(0);
 	gtk_label_set_selectable(GTK_LABEL(copyright), true);
 	gtk_label_set_justify(GTK_LABEL(copyright), GTK_JUSTIFY_CENTER);
