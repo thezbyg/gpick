0d17790fc9c4ea3c363c115e801e531e4838409b
Albertas Vyšniauskas <thezbyg@gmail.com>
2025-05-18 15:26:11 +0300

    Update logo and information in about page.

--- source/uiAbout.cpp
+++ source/uiAbout.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2022, Albertas Vyšniauskas
+ * Copyright (c) 2009-2025, Albertas Vyšniauskas
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -39,7 +39,7 @@ static GtkWidget* new_page(const char *text)
 void show_about_box(GtkWidget *widget)
 {
 	const char *license = {
-"Copyright \xc2\xa9 2009-2022, Albertas Vyšniauskas\n"
+"Copyright \xc2\xa9 2009-2025, Albertas Vyšniauskas\n"
 "\n"
 "All rights reserved.\n"
 "\n"
@@ -99,7 +99,7 @@ void show_about_box(GtkWidget *widget)
 	gtk_label_set_markup(GTK_LABEL(name), tmp_string);
 	gtk_box_pack_start(GTK_BOX(vbox2), name, false, false, 0);
 	g_free(tmp_string);
-	tmp_string = g_markup_printf_escaped("<span size=\"small\">%s © 2009-2022, Albertas Vyšniauskas %s</span>", _("Copyright"), _("and Gpick development team"));
+	tmp_string = g_markup_printf_escaped("<span size=\"small\">%s © 2009-2025, Albertas Vyšniauskas %s</span>", _("Copyright"), _("and Gpick development team"));
 	GtkWidget *copyright = gtk_label_new(0);
 	gtk_label_set_selectable(GTK_LABEL(copyright), true);
 	gtk_label_set_justify(GTK_LABEL(copyright), GTK_JUSTIFY_CENTER);
